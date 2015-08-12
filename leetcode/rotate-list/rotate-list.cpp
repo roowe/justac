46fb4f0a42c39a41daf3adaf1d91a4e7f1a963db
Roowe <bestluoliwe@gmail.com>
2015-08-12 17:27:15 +0800

    Rotate List

---
+++ leetcode/rotate-list/rotate-list.cpp
@@ -0,0 +1,97 @@
+#include<cstdio>
+#include<iostream>
+#include<set>
+#include<map>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<cctype>
+#define LL long long int
+#define INF 0x3f3f3f3f
+#define EPS 1.0e-8
+#define RPE(i,s,e) for(int i = s; i < (int)(e); i++)
+#define FOR(i,c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define MEM(c,i) memset(c, i, sizeof(c)) // i爲0或者-1
+#define QC(q) while(!q.empty())q.pop()
+#define IN(x,low,high) (x>low&&x<high)//low<x<high
+#ifdef ONLINE_JUDGE  //判断是不是OJ系统上编译
+#define debug(format, ...)  0
+#else
+char _red_buff[65535];
+// {debug,     "\e[0;38m" },
+// {info,      "\e[1;37m" },
+// {notice,    "\e[1;36m" },
+// {warning,   "\e[1;33m" },
+// {error,     "\e[1;31m" },
+// {critical,  "\e[1;35m" },
+// {alert,     "\e[1;44m" },
+// {emergency, "\e[1;41m" }
+#define debug(format, ...) strcpy(_red_buff, "\e[1;36m");strcat(_red_buff, format);strcat(_red_buff, "\e[0m\r\n");printf(_red_buff, ##__VA_ARGS__)
+#endif
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode() : val(0), next(NULL) {}    
+};
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) {
+            return NULL;
+        }
+        int len=1;
+        ListNode* cur = head;
+        for(; cur->next; cur=cur->next, len++);
+        cur->next = head;
+        int n = len - k%len;
+        for (int i=0; i<n; i++) {
+            cur = cur->next;
+        }        
+        ListNode* ret = cur->next;
+        cur->next = NULL;
+        return ret;
+    }
+};
+int main()
+{
+    int array[100];
+    int k;
+    int n;
+    ListNode node_list[100];
+    while (cin >> n) {
+        for (int i=0; i<n; i++) {
+            cin >> array[i];
+        }
+        cin >> k;
+        cout << "n: " << n
+             << ", k: " << k
+             << endl;
+        for (int i=0; i<n; i++) {
+            node_list[i].val=array[i];
+            node_list[i].next = &node_list[i+1];
+        }
+        node_list[n-1].next = NULL;
+        ListNode* cur = node_list;
+        while(cur != NULL) {
+            cout << cur << endl;
+            cur = cur->next;
+        }
+        cout << "-------" << endl;
+        Solution s;
+        cur = node_list;
+        cur = s.rotateRight(cur, k);
+        while(cur != NULL) {
+            cout << cur->val << endl;
+            cur = cur->next;
+        }
+        cout << "--------------------分割线--------------------" << endl;
+    }
+    return 0;
+}
+
